c532dff9278a4b5204d64745621d7ef4eb48b571
Deep Mazumder <deep.mazumder23@lpu.in>
2026-02-20 11:32:42 +0530

    Added custom admin dashboard

--- ecom_project/urls.py
+++ ecom_project/urls.py
@@ -14,6 +14,8 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('products/', include('products.urls')),
     path('orders/', include('orders.urls')),
+    path('adminpanel/', include('adminproject.urls')),
+
 ]
 
 if settings.DEBUG:
